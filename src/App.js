95dcc975c795c403619d435dbbcac33201c65bff
Rishit Shivesh <rishitshivesh@gmail.com>
2023-02-11 17:17:31 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
+import "./App.css";
 
-import './App.css';
-
-import { useState, useEffect } from 'react';
-import Navigate from './Navigation';
+import { useState, useEffect } from "react";
+import Navigate from "./Navigation";
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [onBoarding, setIsOnBoarding] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [onBoarding, setIsOnBoarding] = useState(true);
   const handleClick = () => {
     setTimeout(() => {
-
-      const el = document.getElementById('options');
-      console.log(el)
+      const el = document.getElementById(":1.close");
+      console.log(el);
       if (el) {
         el.click();
       }
-    }, 200)
-  }
+    }, 200);
+  };
   // useEffect(() => {
 
   // }, [])
   return (
     <div className="App">
-      <div className="">
-        <div className="" id="google_element" onClick={handleClick}></div>
-      </div>
+      <div className="absolute right-5 top-0"></div>
       <Navigate isLoggedIn={isLoggedIn} onBoarding={onBoarding} />
     </div>
   );
--- src/Constants/routes.constant.js
+++ src/Constants/routes.constant.js
@@ -5,23 +5,22 @@ import LoginPage from "../Pages/Login.page";
 import SignupPage from "../Pages/Signup.page";
 import Razorpay from "../Pages/Razorpay.page";
 export const PRIVATE_ROUTES = [
-
+  {
+    path: "/",
+    component: <HomePage />,
+  },
 ];
 export const PUBLIC_ROUTES = [
-    {
-        path: "/",
-        component: <HomePage />,
-    },
-    {
-        path: "/login",
-        component: <LoginPage />,
-    },
-    {
-        path: "/signup",
-        component: <SignupPage />,
-    },
-    {
-        path: "/razorpay",
-        component: <Razorpay />,
-    },
+  {
+    path: "/login",
+    component: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    component: <SignupPage />,
+  },
+  {
+    path: "/razorpay",
+    component: <Razorpay />,
+  },
 ];
