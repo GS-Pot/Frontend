8676670aaf5c926ca60c7c90c1a27bbe1ebb8461
Rishit Shivesh <rishitshivesh@gmail.com>
2023-02-11 11:35:06 +0530

    razorpay

--- src/Constants/routes.constant.js
+++ src/Constants/routes.constant.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import HomePage from '../Pages/Home.page';
+import React from "react";
+import HomePage from "../Pages/Home.page";
 
-import LoginPage from '../Pages/Login.page';
-import SignupPage from '../Pages/Signup.page';
-
+import LoginPage from "../Pages/Login.page";
+import SignupPage from "../Pages/Signup.page";
+import Razorpay from "../Pages/Razorpay.page";
 export const PRIVATE_ROUTES = [
-    {
-        path: "/",
-        component: <HomePage />
-    },
-
-]
+  {
+    path: "/",
+    component: <HomePage />,
+  },
+];
 export const PUBLIC_ROUTES = [
-    {
-        path: "/login",
-        component: <LoginPage />
-    },
-    {
-        path: "/signup",
-        component: <SignupPage />
-    },
-
-]
+  {
+    path: "/login",
+    component: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    component: <SignupPage />,
+  },
+  {
+    path: "/razorpay",
+    component: <Razorpay />,
+  },
+];
