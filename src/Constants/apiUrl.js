fa6e279ddef280bc0ed1be4321c1d812969bf439
Rishit Shivesh <55372921+rishitshivesh@users.noreply.github.com>
2023-10-13 00:06:08 +0530

    Update apiUrl.js

--- src/Constants/apiUrl.js
+++ src/Constants/apiUrl.js
@@ -1,2 +1,2 @@
 // export const API_URI = ''
-export const API_URI = 'https://trinit-gs-pot-dev-dev-03-backend.vercel.app
+export const API_URI = 'https://trinit-gs-pot-dev-dev-03-backend.vercel.app'
